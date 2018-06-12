f6d40df8be6358b40b7750639e3791880f7a0352
leor8 <leo442183205@gmail.com>
2018-06-12 19:42:59 +0000

    express_server.js example app with response

--- express_server.js
+++ express_server.js
@@ -1,3 +1,11 @@
+/*
+  APPLICATION DESCRIPTION:
+
+  Leo Ruan
+  2018/06/21
+*/
+
+// Requiring the express lib and setting up the port number
 var express = require("express");
 var app = express();
 var PORT = 8080;
@@ -11,6 +19,31 @@ app.get("/", function (req, res){
   res.end("Hello!");
 });
 
+app.get("/urls.json", function (req, res){
+  res.json(urlDatabase);
+});
+
+app.get("/hello", function (req, res){
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
 app.listen(PORT, function (){
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
