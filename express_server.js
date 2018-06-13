7d7867f686b46649a7b06af5559604f3b1bc432a
leor8 <leo442183205@gmail.com>
2018-06-13 17:13:08 +0000

    using cookieparser and a oage with login required

--- express_server.js
+++ express_server.js
@@ -9,6 +9,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cookieParser = require("cookie-parser");
 const PORT = 8080;
 
 // variables
@@ -24,6 +25,7 @@ var urlDatabase = {
 // Setting ejs and bodyParser
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Default GET
 app.get("/", function (req, res){
@@ -36,34 +38,43 @@ app.get("/urls", function (req, res){
   res.render("urls_index", templateVars);
 });
 
-// Rendering the page getting
+// Rendering the page getting new url from user via input field
 app.get("/urls/new", function (req, res){
   res.render("urls_new");
 });
 
-// get url typed in the link
+// Rendering updating input request page that allow user to update
+// the correspondant hyperlink to the URL. Accessing this page from
+// urls_index's edit link
 app.get("/urls/:id", function (req, res){
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/u/:shortURL", function (req, res){
   let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-// PUT url received from /urls/new
+// post url received from /urls/new and redirect to that webpage
+// provided by user and also generate a new id for that hyperlink
+// and save it in the database
 app.post("/urls", function (req, res){
   uid = generateRandomString();
   urlDatabase[uid] = req.body.longURL;
   res.redirect(`u/${uid}`);
 });
 
+// Updated from database page if user hit delete and redirect back
+// to database page with updated database
 app.post("/urls/:id/delete", function (req, res){
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
 
+// If user requested changed the link paired with id and update in
+// database and redirect back to database page with updated database
 app.post("/urls/update/:id", function (req, res){
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls");
